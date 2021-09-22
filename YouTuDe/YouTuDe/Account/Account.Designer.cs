4199508f66dea51c5fff375386d3a87738475ec0
LightNat <91188229+LightNat@users.noreply.github.com>
2021-09-22 21:01:50 +0800

    Upload

---
+++ YouTuDe/YouTuDe/Account/Account.Designer.cs
@@ -0,0 +1,365 @@
+﻿
+namespace YouTuDe.Account
+{
+    partial class Account
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.txtphonenumber = new System.Windows.Forms.TextBox();
+            this.txtpassword = new System.Windows.Forms.TextBox();
+            this.txtusername = new System.Windows.Forms.TextBox();
+            this.txtage = new System.Windows.Forms.TextBox();
+            this.txtlastname = new System.Windows.Forms.TextBox();
+            this.txtfirstname = new System.Windows.Forms.TextBox();
+            this.btnsave = new System.Windows.Forms.Button();
+            this.btnback = new System.Windows.Forms.Button();
+            this.btnchoose = new System.Windows.Forms.Button();
+            this.pbimage = new System.Windows.Forms.PictureBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rdbTourist = new System.Windows.Forms.RadioButton();
+            this.rdbDriver = new System.Windows.Forms.RadioButton();
+            this.label10 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbimage)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Location = new System.Drawing.Point(5, 5);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(529, 339);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.Controls.Add(this.label10);
+            this.panel2.Controls.Add(this.groupBox1);
+            this.panel2.Controls.Add(this.txtphonenumber);
+            this.panel2.Controls.Add(this.txtpassword);
+            this.panel2.Controls.Add(this.txtusername);
+            this.panel2.Controls.Add(this.txtage);
+            this.panel2.Controls.Add(this.txtlastname);
+            this.panel2.Controls.Add(this.txtfirstname);
+            this.panel2.Controls.Add(this.btnsave);
+            this.panel2.Controls.Add(this.btnback);
+            this.panel2.Controls.Add(this.btnchoose);
+            this.panel2.Controls.Add(this.pbimage);
+            this.panel2.Controls.Add(this.label9);
+            this.panel2.Controls.Add(this.label8);
+            this.panel2.Controls.Add(this.label7);
+            this.panel2.Controls.Add(this.label6);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Location = new System.Drawing.Point(5, 5);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(518, 328);
+            this.panel2.TabIndex = 0;
+            // 
+            // txtphonenumber
+            // 
+            this.txtphonenumber.Location = new System.Drawing.Point(363, 146);
+            this.txtphonenumber.Name = "txtphonenumber";
+            this.txtphonenumber.Size = new System.Drawing.Size(114, 20);
+            this.txtphonenumber.TabIndex = 18;
+            // 
+            // txtpassword
+            // 
+            this.txtpassword.Location = new System.Drawing.Point(363, 89);
+            this.txtpassword.Name = "txtpassword";
+            this.txtpassword.Size = new System.Drawing.Size(114, 20);
+            this.txtpassword.TabIndex = 17;
+            // 
+            // txtusername
+            // 
+            this.txtusername.Location = new System.Drawing.Point(363, 62);
+            this.txtusername.Name = "txtusername";
+            this.txtusername.Size = new System.Drawing.Size(114, 20);
+            this.txtusername.TabIndex = 16;
+            // 
+            // txtage
+            // 
+            this.txtage.Location = new System.Drawing.Point(116, 121);
+            this.txtage.Name = "txtage";
+            this.txtage.Size = new System.Drawing.Size(64, 20);
+            this.txtage.TabIndex = 15;
+            // 
+            // txtlastname
+            // 
+            this.txtlastname.Location = new System.Drawing.Point(116, 89);
+            this.txtlastname.Name = "txtlastname";
+            this.txtlastname.Size = new System.Drawing.Size(114, 20);
+            this.txtlastname.TabIndex = 14;
+            // 
+            // txtfirstname
+            // 
+            this.txtfirstname.Location = new System.Drawing.Point(117, 62);
+            this.txtfirstname.Name = "txtfirstname";
+            this.txtfirstname.Size = new System.Drawing.Size(113, 20);
+            this.txtfirstname.TabIndex = 13;
+            // 
+            // btnsave
+            // 
+            this.btnsave.Location = new System.Drawing.Point(422, 295);
+            this.btnsave.Name = "btnsave";
+            this.btnsave.Size = new System.Drawing.Size(75, 23);
+            this.btnsave.TabIndex = 12;
+            this.btnsave.Text = "Save";
+            this.btnsave.UseVisualStyleBackColor = true;
+            this.btnsave.Click += new System.EventHandler(this.btnsave_Click);
+            // 
+            // btnback
+            // 
+            this.btnback.Location = new System.Drawing.Point(13, 295);
+            this.btnback.Name = "btnback";
+            this.btnback.Size = new System.Drawing.Size(36, 23);
+            this.btnback.TabIndex = 11;
+            this.btnback.Text = "←";
+            this.btnback.UseVisualStyleBackColor = true;
+            this.btnback.Click += new System.EventHandler(this.btnback_Click);
+            // 
+            // btnchoose
+            // 
+            this.btnchoose.Location = new System.Drawing.Point(153, 258);
+            this.btnchoose.Name = "btnchoose";
+            this.btnchoose.Size = new System.Drawing.Size(85, 23);
+            this.btnchoose.TabIndex = 10;
+            this.btnchoose.Text = "Upload Image";
+            this.btnchoose.UseVisualStyleBackColor = true;
+            this.btnchoose.Click += new System.EventHandler(this.btnchoose_Click);
+            // 
+            // pbimage
+            // 
+            this.pbimage.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbimage.Location = new System.Drawing.Point(94, 184);
+            this.pbimage.Name = "pbimage";
+            this.pbimage.Size = new System.Drawing.Size(100, 68);
+            this.pbimage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbimage.TabIndex = 9;
+            this.pbimage.TabStop = false;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(341, 148);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(16, 16);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "+";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(270, 123);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(118, 16);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Phone Number: ";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(27, 165);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(61, 16);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Profile: ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(27, 123);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(44, 16);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Age: ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(270, 91);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(84, 16);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Password: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(270, 64);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(87, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Username: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(27, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Lastname: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(27, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Firstname: ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MingLiU-ExtB", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(171, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(166, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "REGISTRATION";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rdbDriver);
+            this.groupBox1.Controls.Add(this.rdbTourist);
+            this.groupBox1.Location = new System.Drawing.Point(347, 216);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(130, 38);
+            this.groupBox1.TabIndex = 19;
+            this.groupBox1.TabStop = false;
+            // 
+            // rdbTourist
+            // 
+            this.rdbTourist.AutoSize = true;
+            this.rdbTourist.Location = new System.Drawing.Point(7, 11);
+            this.rdbTourist.Name = "rdbTourist";
+            this.rdbTourist.Size = new System.Drawing.Size(57, 17);
+            this.rdbTourist.TabIndex = 0;
+            this.rdbTourist.TabStop = true;
+            this.rdbTourist.Text = "Tourist";
+            this.rdbTourist.UseVisualStyleBackColor = true;
+            // 
+            // rdbDriver
+            // 
+            this.rdbDriver.AutoSize = true;
+            this.rdbDriver.Location = new System.Drawing.Point(70, 11);
+            this.rdbDriver.Name = "rdbDriver";
+            this.rdbDriver.Size = new System.Drawing.Size(53, 17);
+            this.rdbDriver.TabIndex = 1;
+            this.rdbDriver.TabStop = true;
+            this.rdbDriver.Text = "Driver";
+            this.rdbDriver.UseVisualStyleBackColor = true;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(270, 197);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(85, 16);
+            this.label10.TabIndex = 20;
+            this.label10.Text = "User Type:";
+            // 
+            // Account
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(222)))), ((int)(((byte)(89)))));
+            this.ClientSize = new System.Drawing.Size(540, 350);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Account";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Account";
+            this.Load += new System.EventHandler(this.Account_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbimage)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pbimage;
+        private System.Windows.Forms.Button btnchoose;
+        private System.Windows.Forms.TextBox txtfirstname;
+        private System.Windows.Forms.Button btnsave;
+        private System.Windows.Forms.Button btnback;
+        private System.Windows.Forms.TextBox txtphonenumber;
+        private System.Windows.Forms.TextBox txtpassword;
+        private System.Windows.Forms.TextBox txtusername;
+        private System.Windows.Forms.TextBox txtage;
+        private System.Windows.Forms.TextBox txtlastname;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rdbDriver;
+        private System.Windows.Forms.RadioButton rdbTourist;
+    }
+}
---
+++ YouTuDe/YouTuDe/Connection/Connection.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace YouTuDe.Connection
+{
+    class Connection
+    {
+        public static SqlConnection conn;
+        private static string connect = "Data Source=DESKTOP-Q4Sl8VR;Initial Catalog=YouTuDe;Integrated Security=True";
+
+        public static void DB()
+        {
+            try
+            {
+                conn = new SqlConnection(connect);
+                conn.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
