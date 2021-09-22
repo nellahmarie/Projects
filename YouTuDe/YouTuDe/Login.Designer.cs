aa8e9d49471b6381407538ac26be09f3dc25cf53
LightNat <91188229+LightNat@users.noreply.github.com>
2021-09-22 20:56:05 +0800

    Add files via upload

---
+++ YouTuDe/YouTuDe/Login.Designer.cs
@@ -0,0 +1,185 @@
+﻿
+namespace YouTuDe
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.linkLabelForgot = new System.Windows.Forms.LinkLabel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtpassword = new System.Windows.Forms.TextBox();
+            this.linkLabelCreate = new System.Windows.Forms.LinkLabel();
+            this.btnlogin = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtusername = new System.Windows.Forms.TextBox();
+            this.pbLogo = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbLogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Location = new System.Drawing.Point(5, 5);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(310, 335);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.Controls.Add(this.linkLabelForgot);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.txtpassword);
+            this.panel2.Controls.Add(this.linkLabelCreate);
+            this.panel2.Controls.Add(this.btnlogin);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.txtusername);
+            this.panel2.Controls.Add(this.pbLogo);
+            this.panel2.Location = new System.Drawing.Point(5, 5);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(299, 323);
+            this.panel2.TabIndex = 0;
+            // 
+            // linkLabelForgot
+            // 
+            this.linkLabelForgot.AutoSize = true;
+            this.linkLabelForgot.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabelForgot.Location = new System.Drawing.Point(183, 252);
+            this.linkLabelForgot.Name = "linkLabelForgot";
+            this.linkLabelForgot.Size = new System.Drawing.Size(81, 12);
+            this.linkLabelForgot.TabIndex = 13;
+            this.linkLabelForgot.TabStop = true;
+            this.linkLabelForgot.Text = "Forgot Password?";
+            this.linkLabelForgot.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabelForgot_LinkClicked);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(121, 223);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Password";
+            // 
+            // txtpassword
+            // 
+            this.txtpassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtpassword.Location = new System.Drawing.Point(87, 194);
+            this.txtpassword.Name = "txtpassword";
+            this.txtpassword.Size = new System.Drawing.Size(132, 26);
+            this.txtpassword.TabIndex = 10;
+            // 
+            // linkLabelCreate
+            // 
+            this.linkLabelCreate.AutoSize = true;
+            this.linkLabelCreate.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabelCreate.Location = new System.Drawing.Point(18, 296);
+            this.linkLabelCreate.Name = "linkLabelCreate";
+            this.linkLabelCreate.Size = new System.Drawing.Size(59, 12);
+            this.linkLabelCreate.TabIndex = 9;
+            this.linkLabelCreate.TabStop = true;
+            this.linkLabelCreate.Text = "No Account?";
+            this.linkLabelCreate.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabelCreate_LinkClicked);
+            // 
+            // btnlogin
+            // 
+            this.btnlogin.Location = new System.Drawing.Point(207, 286);
+            this.btnlogin.Name = "btnlogin";
+            this.btnlogin.Size = new System.Drawing.Size(75, 23);
+            this.btnlogin.TabIndex = 8;
+            this.btnlogin.Text = "Login";
+            this.btnlogin.UseVisualStyleBackColor = true;
+            this.btnlogin.Click += new System.EventHandler(this.btnlogin_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(121, 166);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Username";
+            // 
+            // txtusername
+            // 
+            this.txtusername.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtusername.Location = new System.Drawing.Point(87, 137);
+            this.txtusername.Name = "txtusername";
+            this.txtusername.Size = new System.Drawing.Size(132, 26);
+            this.txtusername.TabIndex = 6;
+            // 
+            // pbLogo
+            // 
+            this.pbLogo.Location = new System.Drawing.Point(21, -51);
+            this.pbLogo.Name = "pbLogo";
+            this.pbLogo.Size = new System.Drawing.Size(261, 214);
+            this.pbLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbLogo.TabIndex = 12;
+            this.pbLogo.TabStop = false;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(222)))), ((int)(((byte)(89)))));
+            this.ClientSize = new System.Drawing.Size(321, 346);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Login_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbLogo)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pbLogo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtpassword;
+        private System.Windows.Forms.LinkLabel linkLabelCreate;
+        private System.Windows.Forms.Button btnlogin;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtusername;
+        private System.Windows.Forms.LinkLabel linkLabelForgot;
+    }
+}
+
---
+++ YouTuDe/YouTuDe/Login.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace YouTuDe
+{
+    public partial class Login : Form
+    {
+        //Personal Info
+        public static string userid;
+        public static string firstname;
+        public static string lastname;
+        public static string age;
+        public static string accountSid;
+        public static string authToken;
+        public static string accountNumber;
+        public static string profile;
+        public static string userNumber;
+        public static string username;
+        public static string password;
+        public static string rolename;
+
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+            var YouTude = Path.GetDirectoryName(Application.ExecutablePath) + "\\Image\\YouTuDe.png";
+            pbLogo.Image = Image.FromFile(YouTude);
+        }
+
+        private void linkLabelCreate_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Account.Account account = new Account.Account();
+            this.Visible = false;
+            account.Show();
+        }
+
+        private void linkLabelForgot_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+
+        }
+
+        private void btnlogin_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(txtusername.Text) == true)
+            {
+                MessageBox.Show("Username must not be set as Null or Empty", "Null | Empty", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if(String.IsNullOrEmpty(txtpassword.Text) == true)
+            {
+                MessageBox.Show("Password must not be set as Null or Empty", "Null | Empty", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                try
+                {
+                    Connection.Connection.DB();
+                    Function.Function.gen = "SELECT * FROM users WHERE username = '"+txtusername.Text+"' AND password = '"+txtpassword.Text+"' ";
+                    Function.Function.command = new SqlCommand(Function.Function.gen, Connection.Connection.conn);
+                    Function.Function.reader = Function.Function.command.ExecuteReader();
+
+                    if (Function.Function.reader.HasRows)
+                    {
+                        Function.Function.reader.Read();
+
+                        txtusername.Text = Function.Function.reader.GetValue(9).ToString();
+                        txtpassword.Text = Function.Function.reader.GetValue(10).ToString();
+
+                        //Full Info
+                        userid = Function.Function.reader.GetValue(0).ToString();
+                        firstname = Function.Function.reader.GetValue(1).ToString();
+                        lastname = Function.Function.reader.GetValue(2).ToString();
+                        age = Function.Function.reader.GetValue(3).ToString();
+                        accountSid = Function.Function.reader.GetValue(4).ToString();
+                        authToken = Function.Function.reader.GetValue(5).ToString();
+                        accountNumber = Function.Function.reader.GetValue(6).ToString();
+                        profile = Function.Function.reader.GetValue(7).ToString();
+                        userNumber = Function.Function.reader.GetValue(8).ToString();
+                        username = txtusername.Text;
+                        password = txtpassword.Text;
+                        rolename = Function.Function.reader.GetValue(11).ToString();
+
+                        if (rolename == "Tourist")
+                        {
+                            Client.Dashboard dashboard = new Client.Dashboard();
+                            this.Visible = false;
+                            dashboard.Show();
+                        }
+                        else if (rolename == "Driver")
+                        {
+                            MessageBox.Show("Driver");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Manager");
+                        }
+                    }
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+    }
+}
