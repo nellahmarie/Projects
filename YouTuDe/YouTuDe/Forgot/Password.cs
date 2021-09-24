fac7fa3821bb78ae57397498e959729870618495
LightNat <91188229+LightNat@users.noreply.github.com>
2021-09-24 22:24:12 +0800

    full update reset password

--- YouTuDe/YouTuDe/Forgot/Password.cs
+++ YouTuDe/YouTuDe/Forgot/Password.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -49,7 +50,25 @@ namespace YouTuDe.Forgot
 
         private void btnchangepassword_Click(object sender, EventArgs e)
         {
-            
+            try
+            {
+                Connection.Connection.DB();
+                Function.Function.gen = "UPDATE users SET password = '"+txtpassword2.Text+"' WHERE userid = '"+Id.userid+"' ";
+                Function.Function.command = new SqlCommand(Function.Function.gen, Connection.Connection.conn);
+                Function.Function.command.ExecuteNonQuery();
+
+                Login login = new Login();
+                this.Visible = false;
+                login.Show();
+
+                MessageBox.Show("Password Changed Successfully!", "Password", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                Connection.Connection.conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
--- YouTuDe/YouTuDe/Login.Designer.cs
+++ YouTuDe/YouTuDe/Login.Designer.cs
@@ -96,6 +96,7 @@ namespace YouTuDe
             this.txtpassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtpassword.Location = new System.Drawing.Point(87, 194);
             this.txtpassword.Name = "txtpassword";
+            this.txtpassword.PasswordChar = '*';
             this.txtpassword.Size = new System.Drawing.Size(132, 26);
             this.txtpassword.TabIndex = 10;
             // 
