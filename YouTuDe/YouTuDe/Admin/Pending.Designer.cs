1e23184f738def72e5e2d0882c58322afcc6416a
LightNat <91188229+LightNat@users.noreply.github.com>
2021-09-26 18:58:03 +0800

    Update

---
+++ YouTuDe/YouTuDe/Admin/Pending.Designer.cs
@@ -0,0 +1,269 @@
+﻿
+namespace YouTuDe.Admin
+{
+    partial class Pending
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblfullname = new System.Windows.Forms.Label();
+            this.pbprofile = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbprofile)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(33)))), ((int)(((byte)(32)))));
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.button8);
+            this.panel1.Controls.Add(this.button7);
+            this.panel1.Controls.Add(this.button6);
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.lblfullname);
+            this.panel1.Controls.Add(this.pbprofile);
+            this.panel1.Location = new System.Drawing.Point(5, 5);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(167, 448);
+            this.panel1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(53, 418);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Manager";
+            // 
+            // button8
+            // 
+            this.button8.FlatAppearance.BorderSize = 0;
+            this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(222)))), ((int)(((byte)(89)))));
+            this.button8.Location = new System.Drawing.Point(-1, 377);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(167, 34);
+            this.button8.TabIndex = 14;
+            this.button8.Text = "LOGOUT";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.FlatAppearance.BorderSize = 0;
+            this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(222)))), ((int)(((byte)(89)))));
+            this.button7.Location = new System.Drawing.Point(-1, 345);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(167, 34);
+            this.button7.TabIndex = 13;
+            this.button7.Text = "REPORTS";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.FlatAppearance.BorderSize = 0;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(222)))), ((int)(((byte)(89)))));
+            this.button6.Location = new System.Drawing.Point(-1, 313);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(167, 34);
+            this.button6.TabIndex = 12;
+            this.button6.Text = "STATUS";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.FlatAppearance.BorderSize = 0;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(222)))), ((int)(((byte)(89)))));
+            this.button5.Location = new System.Drawing.Point(-1, 281);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(167, 34);
+            this.button5.TabIndex = 11;
+            this.button5.Text = "PENDING";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(222)))), ((int)(((byte)(89)))));
+            this.button4.Location = new System.Drawing.Point(-1, 249);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(167, 34);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "DRIVERS";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(222)))), ((int)(((byte)(89)))));
+            this.button3.Location = new System.Drawing.Point(-1, 217);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(167, 34);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "TOURIST";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(222)))), ((int)(((byte)(89)))));
+            this.button2.Location = new System.Drawing.Point(-1, 185);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(167, 34);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "SPOTS";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(222)))), ((int)(((byte)(89)))));
+            this.button1.Location = new System.Drawing.Point(-1, 153);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(167, 34);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "DASHBOARD";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // lblfullname
+            // 
+            this.lblfullname.AutoSize = true;
+            this.lblfullname.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblfullname.ForeColor = System.Drawing.Color.Red;
+            this.lblfullname.Location = new System.Drawing.Point(67, 114);
+            this.lblfullname.Name = "lblfullname";
+            this.lblfullname.Size = new System.Drawing.Size(28, 16);
+            this.lblfullname.TabIndex = 6;
+            this.lblfullname.Text = "FN";
+            // 
+            // pbprofile
+            // 
+            this.pbprofile.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(225)))), ((int)(((byte)(106)))));
+            this.pbprofile.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbprofile.Location = new System.Drawing.Point(31, 17);
+            this.pbprofile.Name = "pbprofile";
+            this.pbprofile.Size = new System.Drawing.Size(100, 83);
+            this.pbprofile.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbprofile.TabIndex = 0;
+            this.pbprofile.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(313, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "PENDING";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Black;
+            this.panel2.Controls.Add(this.panel3);
+            this.panel2.Location = new System.Drawing.Point(178, 127);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(363, 324);
+            this.panel2.TabIndex = 4;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.White;
+            this.panel3.Location = new System.Drawing.Point(5, 5);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(353, 313);
+            this.panel3.TabIndex = 0;
+            // 
+            // Pending
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(222)))), ((int)(((byte)(89)))));
+            this.ClientSize = new System.Drawing.Size(553, 456);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Pending";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Pending";
+            this.Load += new System.EventHandler(this.Pending_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbprofile)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pbprofile;
+        private System.Windows.Forms.Label lblfullname;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+    }
+}
---
+++ YouTuDe/YouTuDe/Admin/Reports.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YouTuDe.Admin
+{
+    public partial class Reports : Form
+    {
+        private int id;
+        private string profile;
+
+        //for string count
+        int count;
+        string name;
+
+        public Reports()
+        {
+            InitializeComponent();
+        }
+
+        private void Reports_Load(object sender, EventArgs e)
+        {
+            id = Convert.ToInt32(Login.userid);
+
+            displayProfile();
+
+            Allignment();
+
+            lblfullname.Text = Login.firstname + " " + Login.lastname;
+        }
+
+        public void Allignment()
+        {
+            name = Login.firstname + " " + Login.lastname;
+            count = name.Length;
+
+            if (count == 20)
+            {
+                this.lblfullname.Location = new Point(-4, 114);
+            }
+            else if (count == 19)
+            {
+                this.lblfullname.Location = new Point(0, 114);
+            }
+            else if (count == 18)
+            {
+                this.lblfullname.Location = new Point(4, 114);
+            }
+            else if (count == 17)
+            {
+                this.lblfullname.Location = new Point(8, 114);
+            }
+            else if (count == 16)
+            {
+                this.lblfullname.Location = new Point(12, 114);
+            }
+            else if (count == 15)
+            {
+                this.lblfullname.Location = new Point(16, 114);
+            }
+            else if (count == 14)
+            {
+                this.lblfullname.Location = new Point(20, 114);
+            }
+            else if (count == 13)
+            {
+                this.lblfullname.Location = new Point(24, 114);
+            }
+            else if (count == 12)
+            {
+                this.lblfullname.Location = new Point(28, 114);
+            }
+            else if (count == 11)
+            {
+                this.lblfullname.Location = new Point(32, 114);
+            }
+            else if (count == 10)
+            {
+                this.lblfullname.Location = new Point(36, 114);
+            }
+            else if (count == 9)
+            {
+                this.lblfullname.Location = new Point(40, 114);
+            }
+            else if (count == 8)
+            {
+                this.lblfullname.Location = new Point(44, 114);
+            }
+            else if (count == 7)
+            {
+                this.lblfullname.Location = new Point(48, 114);
+            }
+            else if (count == 6)
+            {
+                this.lblfullname.Location = new Point(52, 114);
+            }
+            else if (count == 5)
+            {
+                this.lblfullname.Location = new Point(56, 114);
+            }
+            else if (count == 4)
+            {
+                this.lblfullname.Location = new Point(60, 114);
+            }
+            else if (count == 3)
+            {
+                this.lblfullname.Location = new Point(64, 114);
+            }
+
+        }
+
+        public void displayProfile()
+        {
+            try
+            {
+                Connection.Connection.DB();
+                Function.Function.gen = "SELECT * FROM users WHERE userid = '" + id + "' ";
+                Function.Function.command = new SqlCommand(Function.Function.gen, Connection.Connection.conn);
+                Function.Function.reader = Function.Function.command.ExecuteReader();
+
+                if (Function.Function.reader.HasRows)
+                {
+                    Function.Function.reader.Read();
+
+                    profile = Function.Function.reader.GetValue(7).ToString();
+
+                    try
+                    {
+                        var image = Path.GetDirectoryName(Application.ExecutablePath) + "\\Profile\\" + profile;
+                        pbprofile.Image = Image.FromFile(image);
+                    }
+                    catch (Exception ex)
+                    {
+
+                    }
+                    
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
